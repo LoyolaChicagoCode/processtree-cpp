87ff26ab2cfade4f882c8996747363c4d2ef50b2
Konstantin Läufer <laufer@cs.luc.edu>
2011-10-25 17:15:06 -0500

    can now choose between cstdio and iostream (25% overhead)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <cstdio>  // popen, fgets
+//#define USE_IOSTREAM 1
+
 #include <cstring> // strtok
-#include <iostream>
+#ifdef USE_IOSTREAM
+	#include <iostream>
+#else
+	#include <cstdio>  // popen, fgets
+#endif
 #include <map>
 
 #include "process.h"
@@ -14,8 +19,17 @@ const unsigned int BUF_SIZE = 1 << 16;
 void print_tree(map<int, process>& m, multimap<int, int>& t, int i, int l) {
 	// indent, then print current process
 	for (int k = 0; k < l; k++)
+#ifdef USE_IOSTREAM
 		std::cout << ' ';
+#else
+		putchar(' ');
+#endif
+#ifdef USE_IOSTREAM
 	std::cout << m[i] << '\n';
+#else
+	printf("%d: ", m[i].pid);
+	puts(m[i].cmd.c_str());
+#endif
 	// print children indented by one more level
 	for (multimap<int, int>::iterator e = t.lower_bound(i); e != t.upper_bound(i); e++)
 		print_tree(m, t, e->second, l + 1);
@@ -27,11 +41,19 @@ int main(int argc, char* argv[]) {
 	multimap<int, int> t;
 
 	// analyze header line
+#ifdef USE_IOSTREAM
 	std::cin.getline(buf, sizeof(buf));
+#else
+	fgets(buf, sizeof(buf), stdin);
+#endif
 	Format_Info fi(buf);
 
 	// read lines, parse to process object, and insert into table
+#ifdef USE_IOSTREAM
 	while (std::cin.getline(buf, sizeof(buf)) != NULL) {
+#else
+	while (fgets(buf, sizeof(buf), stdin) != NULL) {
+#endif
 		process proc(buf, fi);
 		m.insert(pair<int, process>(proc.pid, proc));
 	}
