e777aab187f08d53b05736ef265b87b48e4451a8
Konstantin Läufer <laufer@cs.luc.edu>
2022-02-03 18:52:34 -0600

    made code a bit more const-correct
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ using std::getline;
 typedef unordered_map<unsigned int, string> cmd_map;
 typedef unordered_map<unsigned int, vector<unsigned int> > ppid_map;
 
-void print_tree(cmd_map& m, ppid_map& t, unsigned int i, unsigned int l) {
+void print_tree(const cmd_map& m, const ppid_map& t, const unsigned int i, const unsigned int l) {
     // indent, then print current process
-    fmt::print("{} {}: {}\n", string(l, ' '), i, m[i]);
+    fmt::print("{} {}: {}\n", string(l, ' '), i, m.at(i));
     // print children indented by one more level
-    for (auto e = t[i].begin(); e != t[i].end(); e++)
+    for (auto e = t.at(i).begin(); e != t.at(i).end(); e++)
         print_tree(m, t, *e, l + 1);
 }
 
-int main(int argc, char* argv[]) {
+int main(const int argc, const char* const argv[]) {
     cmd_map m;
     ppid_map t;
 
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_level(spdlog::level::info);
 
     string line;
     getline(cin, line);
