c9ec297102c89d9fa084cb4f0ac194d32e1119ee
Konstantin Läufer <laufer@cs.luc.edu>
2022-02-11 10:01:56 -0600

    added a few more tests
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- processtree_tests.cpp
+++ processtree_tests.cpp
@@ -10,11 +10,48 @@ EXPECT_STRNE("hello", "world");
 EXPECT_EQ(7 * 6, 42);
 }
 
-TEST(ProcessTreeTests, Parser1) {
+TEST(ProcessTreeTests, ParserHeaderBad1) {
     try {
         process_parser p("a b c");
         FAIL();
     } catch (const int e) {
         SUCCEED();
     }
+}
+
+TEST(ProcessTreeTests, ParserHeaderBad2) {
+    try {
+        process_parser p("a CMD b PID c PPID d");
+        FAIL();
+    } catch (const int e) {
+        SUCCEED();
+    }
+}
+
+TEST(ProcessTreeTests, ParserHeaderBad3) {
+    try {
+        process_parser p("a COMMAND b PID c PPID d");
+        FAIL();
+    } catch (const int e) {
+        SUCCEED();
+    }
+}
+
+TEST(ProcessTreeTests, ParserHeaderBad4) {
+    try {
+        process_parser p("a PID c PPID d CMD e");
+        FAIL();
+    } catch (const int e) {
+        SUCCEED();
+    }
+}
+
+TEST(ProcessTreeTests, ParserHeaderGood1) {
+    //                     01234567890123456789012345678901234
+    process_parser p("a bb PID cccc PPID dddd eee COMMAND");
+}
+
+TEST(ProcessTreeTests, ParserHeaderGood2) {
+    //                     01234567890123456789012345678901234
+    process_parser p("a bb PID cccc PPID dddd eee CMD");
 }
