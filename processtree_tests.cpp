b473dffff6750d88f0cfda3b0b1a4bce49a677ff
Konstantin Läufer <laufer@cs.luc.edu>
2022-02-11 18:02:31 -0600

    added parser tests and converted into proper suite
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- processtree_tests.cpp
+++ processtree_tests.cpp
@@ -1,57 +1,79 @@
+#include <spdlog/spdlog.h>
 #include <gtest/gtest.h>
 
 #include "parsing.h"
 
-// Demonstrate some basic assertions.
-TEST(ProcessTreeTests, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
-}
+class ParserTest : public testing::Test {
+protected:
+    static void SetUpTestSuite() {
+        spdlog::set_level(spdlog::level::debug);
+        spdlog::info("spdlog enabled");
+    }
 
-TEST(ProcessTreeTests, ParserHeaderBad1) {
-    try {
+    static void TearDownTestSuite() {
+    }
+};
+
+TEST_F(ParserTest, ParserHeaderBad1) {
+    EXPECT_ANY_THROW(
         process_parser p("a b c");
-        FAIL();
-    } catch (const int e) {
-        SUCCEED();
-    }
+    );
 }
 
-TEST(ProcessTreeTests, ParserHeaderBad2) {
-    try {
+TEST_F(ParserTest, ParserHeaderBad2) {
+    EXPECT_ANY_THROW(
         process_parser p("a CMD b PID c PPID d");
-        FAIL();
-    } catch (const int e) {
-        SUCCEED();
-    }
+    );
 }
 
-TEST(ProcessTreeTests, ParserHeaderBad3) {
-    try {
+TEST_F(ParserTest, ParserHeaderBad3) {
+    EXPECT_ANY_THROW(
         process_parser p("a COMMAND b PID c PPID d");
-        FAIL();
-    } catch (const int e) {
-        SUCCEED();
-    }
+    );
 }
 
-TEST(ProcessTreeTests, ParserHeaderBad4) {
-    try {
+TEST_F(ParserTest, ParserHeaderBad4) {
+    EXPECT_ANY_THROW(
         process_parser p("a PID c PPID d CMD e");
-        FAIL();
-    } catch (const int e) {
-        SUCCEED();
-    }
+    );
 }
 
-TEST(ProcessTreeTests, ParserHeaderGood1) {
+TEST_F(ParserTest, ParserHeaderGood1) {
     //                     01234567890123456789012345678901234
     process_parser p("a bb PID cccc PPID dddd eee COMMAND");
+    EXPECT_EQ(p.pid, 2);
+    EXPECT_EQ(p.ppid, 4);
+    EXPECT_EQ(p.cmd, 28);
 }
 
-TEST(ProcessTreeTests, ParserHeaderGood2) {
+TEST_F(ParserTest, ParserHeaderGood2) {
     //                     01234567890123456789012345678901234
     process_parser p("a bb PID cccc PPID dddd eee CMD");
+    EXPECT_EQ(p.pid, 2);
+    EXPECT_EQ(p.ppid, 4);
+    EXPECT_EQ(p.cmd, 28);
+}
+
+TEST_F(ParserTest, ParserHeaderGood3) {
+    //                     01234567890123456789012345678901234
+    process_parser p("a bb PPID cccc PID dddd eee COMMAND");
+    EXPECT_EQ(p.pid, 4);
+    EXPECT_EQ(p.ppid, 2);
+    EXPECT_EQ(p.cmd, 28);
+}
+
+TEST_F(ParserTest, ParserHeaderGood4) {
+    //                     01234567890123456789012345678901234
+    process_parser p("a bb PPID cccc PID dddd eee CMD");
+    EXPECT_EQ(p.pid, 4);
+    EXPECT_EQ(p.ppid, 2);
+    EXPECT_EQ(p.cmd, 28);
+}
+
+TEST_F(ParserTest, ParserHeaderGood5) {
+    //                     01234567890123456789012345678901234567890123456789
+    process_parser p("  UID   PID  PPID   C STIME   TTY           TIME CMD");
+    EXPECT_EQ(p.pid, 1);
+    EXPECT_EQ(p.ppid, 2);
+    EXPECT_EQ(p.cmd, 49);
 }
