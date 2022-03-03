0afa2e471ab51b0740381af4a808c205fdc88630
Konstantin Läufer <laufer@cs.luc.edu>
2022-03-03 16:30:08 -0600

    simplified parser into struct, now throwing standard exceptions, fixes #10, fixes #12
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- main.cpp
+++ main.cpp
@@ -1,55 +1,80 @@
-#include <iostream>
 #include <vector>
 #include <forward_list>
 #include <unordered_map>
+#include <algorithm>
 #include <chrono>
-#include <fmt/format.h>
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <stdexcept>
+
 #include <CLI/CLI.hpp>
 #include <magic_enum.hpp>
+#include <fmt/format.h>
+#include <scn/scn.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
-#include "parsing.h"
-
-using std::string;
-using std::string_view;
-using std::pair;
-using std::unordered_map;
-using std::vector;
-using std::forward_list;
-using std::cin;
-using std::getline;
-
-using std::chrono::steady_clock;
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
+using std::string, std::string_view;
+using std::pair, std::unordered_map, std::vector, std::forward_list;
 
 typedef unordered_map<unsigned int, const string> cmd_map;
 typedef unordered_map<unsigned int, vector<unsigned int> > ppid_map;
 
-// can use const pair with clang++ but not g++
-typedef vector<pair<const string, const steady_clock::time_point> > ts_vector;
+// parsing info from header row of ps command
+struct parser {
+    long pid, ppid;    // field numbers of PID and PPID in header row
+    unsigned long cmd; // column of CMD or COMMAND field in header row
+};
 
-void read_input(string& header, forward_list<string>& lines) {
-    getline(cin, header);
+// representation of a single process
+struct process {
+    long pid, ppid;
+    string cmd;
+};
+
+// can use const pair with clang++ but not g++
+typedef vector<pair<const string, const std::chrono::steady_clock::time_point> > ts_vector;
+
+void read_input(std::istream& input, string& header, forward_list<string>& lines) {
+    std::getline(input, header);
     string line;
     auto count = 0;
-    while (getline(cin, line)) {
-        spdlog::debug("input line {}", line);
+    while (std::getline(input, line)) {
         lines.push_front(line);
         count++;
     }
     spdlog::debug("{} lines read", count);
 }
 
-void parse_lines(const string& header, const forward_list<string>& lines, forward_list<process>& processes) {
-    const process_parser parser(header);
+parser parse_header(const string& header) {
+    spdlog::debug("header string {}", header);
+    vector<string> tokens;
+    scn::scan_list(header, tokens);
+    const auto first = tokens.begin();
+    const auto last = tokens.end();
+    const auto pid = find(first, last, "PID") - first;
+    if (pid >= tokens.size()) throw std::runtime_error("required header field PID missing!");
+    const auto ppid = find(first, last, "PPID") - first;
+    if (ppid >= tokens.size()) throw std::runtime_error("required header field PPID missing!");
+    const auto cmd = std::min(header.find("CMD"), header.find("COMMAND"));
+    const auto& rightmost = tokens[tokens.size() - 1];
+    if (rightmost != "CMD" && rightmost != "COMMAND") throw std::runtime_error("required header field CMD or COMMAND missing or not last!");
+    spdlog::debug("header columns {} {} {} {}", pid, ppid, cmd, tokens.size());
+    return { pid, ppid, cmd };
+}
+
+void parse_lines(const parser& parser, const forward_list<string>& lines, forward_list<process>& processes) {
+    vector<string> tokens;
     auto count = 0;
-    spdlog::debug("starting to parse processes");
     for (const auto& line: lines) {
-        processes.push_front(parser.parse(line));
-        count++;
-    };
+        tokens.clear();
+        scn::scan_list(line.substr(0, parser.cmd), tokens);
+        process proc{
+                std::stoi(tokens[parser.pid]),
+                std::stoi(tokens[parser.ppid]),
+                line.substr(parser.cmd)
+        };
+        processes.push_front(proc);
+        count ++;
+    }
     spdlog::debug("{} processes parsed", count);
 }
 
@@ -81,10 +106,12 @@ void print_forest(FILE* const dest, const cmd_map& m, const ppid_map& t) {
 }
 
 void mark_time(ts_vector& timestamps, const string_view label) {
-    timestamps.push_back(pair(label.data(), steady_clock::now()));
+    timestamps.push_back(pair(label.data(), std::chrono::steady_clock::now()));
 }
 
 void print_timestamps(const ts_vector& timestamps) {
+    using std::chrono::milliseconds;
+    using std::chrono::duration_cast;
     for (auto t = timestamps.begin() + 1; t != timestamps.end(); t++) {
         const auto dur = duration_cast<milliseconds>(t->second - (t - 1)->second).count();
         spdlog::info("{}: {} ms", t->first, dur);
@@ -131,9 +158,10 @@ int main(const int argc, const char* const argv[]) {
     ppid_map t;
 
     mark_time(timestamps, "Start time");
-    read_input(header, lines);
+    read_input(std::cin, header, lines);
     mark_time(timestamps, "Input time");
-    parse_lines(header, lines, processes);
+    auto parser = parse_header(header);
+    parse_lines(parser, lines, processes);
     mark_time(timestamps, "Parsing time");
     insert_processes(processes, m, t);
     mark_time(timestamps, "Insertion time");
